1c0520246fdbb34ff0b68caaeec1db9e45db05d7
ChessMasterr <artiom.farhutdinoff@yandex.ru>
2023-02-24 16:37:56 +0500

    did it

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this.min = min;
+    this.max = max;
+    // console.log("входные " + this.min);
+    // console.log("входные " + this.max);
+  }
 
-    }
+  guess() {
+    return (this.a = Math.round((this.max + this.min) / 2));
+    // console.log("гес " + this.a);
+  }
 
-    guess() {
+  lower() {
+    this.max = this.a;
+    // console.log("меньше " + this.a);
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.min = this.a;
+    // console.log("больше " + this.a);
+  }
 }
 
 module.exports = GuessingGame;
